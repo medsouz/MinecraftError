89d1fad0ce5b8c2d283f07902f1afe655353134f
Kane Y <kanepyork@gmail.com>
2012-04-22 10:23:57 -0700

    Made build script more verbose

--- build.sh
+++ build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 echo "Please specify the version number, followed by ENTER. (e.g. 25)"
 read version
-
-#make parents too
+# make directory so javac doesn't have to
 mkdir -p buildtemp/minecrafterror/resources
-#compile
+echo "Compiling"
+# compile
 javac -d buildtemp/ src/minecrafterror/*.java
 if [ $? -ne 0 ]
 then
-	exit 1 #on build fail
+	exit 1 # on build fail
 fi
-#copy resources
+# copy resources
 cp src/minecrafterror/resources/* buildtemp/minecrafterror/resources/
 # remove any old jarfile, make a new one
-#the cd is because jar is weird.
+# the cd is because the jar utility is weird.
 rm MCErrorV${version}.jar
 cd buildtemp
 jar cvfe ../MCErrorV${version}.jar minecrafterror.Main .
 cd ..
 #make jarfile executable
 chmod ugo+x MCErrorV${version}.jar
+echo "Cleaning up"
+rm -r buildtemp/
+echo "Complete"
